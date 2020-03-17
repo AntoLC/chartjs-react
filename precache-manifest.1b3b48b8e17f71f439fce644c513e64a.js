89f03bff793d3cd058ca2dacfce86c22818b9d5d
Anthony Le Courric <freelance.anthony@gmail.com>
2020-03-17 13:00:26 +0100

    Updates

---
+++ precache-manifest.1b3b48b8e17f71f439fce644c513e64a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa1149f1a4b0b2b115702f0dfd235e4",
+    "url": "/chartjs-react/build/index.html"
+  },
+  {
+    "revision": "e1472b610cc78086bce4",
+    "url": "/chartjs-react/build/static/css/main.3aaf1634.chunk.css"
+  },
+  {
+    "revision": "ce5088757ebcdbf64995",
+    "url": "/chartjs-react/build/static/js/2.78b38183.chunk.js"
+  },
+  {
+    "revision": "d2cf8d37946cd02abf2fe73b5ccb09c4",
+    "url": "/chartjs-react/build/static/js/2.78b38183.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1472b610cc78086bce4",
+    "url": "/chartjs-react/build/static/js/main.c3caccb9.chunk.js"
+  },
+  {
+    "revision": "fd7e2cfa98b6ff812408",
+    "url": "/chartjs-react/build/static/js/runtime-main.28eea1fe.js"
+  }
+]);
